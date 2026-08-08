a5d8eabd1c5cb6922fdd9537cdcfca26bffaf603
html.rickey@gmail.com <html.rickey@gmail.com>
2026-08-09 00:56:22 +0900

    アラートの追加

--- assets/js/eart.js
+++ assets/js/eart.js
@@ -16,16 +16,75 @@ async function getEarthquake() {
 
     const earthquake = data[0].earthquake;
 
-    document.getElementById("eart-info").textContent =
-        earthquake.hypocenter.name;
+    const eart = document.getElementById("eart-i");
 
-    document.getElementById("eart-note").textContent =
-        `M${earthquake.hypocenter.magnitude} / 最大震度${getShindo(earthquake.maxScale)}`;
-    document.getElementById("eart-time").textContent =
-        new Date(earthquake.time).toLocaleString("ja-JP", {
-            timeZone: "Asia/Tokyo",
-            hour12: false,
-        });
+    if (earthquake) {
+
+        // 地震情報
+        document.getElementById("eart-info").innerHTML =
+            `揺れを検知した地域：${earthquake.hypocenter.name}`;
+
+        document.getElementById("eart-note").textContent =
+            `M${earthquake.hypocenter.magnitude} / 最大震度${getShindo(earthquake.maxScale)}`;
+
+        document.getElementById("eart-time").textContent =
+            new Date(earthquake.time).toLocaleString("ja-JP", {
+                timeZone: "Asia/Tokyo",
+                hour12: false,
+            });
+
+        eart.style.display = "block";
+
+
+        // 最大震度
+        const shindo = earthquake.maxScale;
+
+        let message = "";
+
+        if (shindo >= 70) {
+
+            message =
+                "🚨 震度7の非常に強い揺れです。津波などの情報を確認し、安全を確保してください。";
+
+        } else if (shindo >= 60) {
+
+            message =
+                "🚨 震度6強の非常に強い揺れです。津波などの情報を確認し、安全を確保してください。";
+
+        } else if (shindo >= 55) {
+
+            message =
+                "⚠️ 震度6弱の強い揺れです。津波などの情報を確認してご注意ください。";
+
+        } else if (shindo >= 50) {
+
+            message =
+                "⚠️ 震度5強の強い揺れです。落下物などに注意してください。";
+
+        } else if (shindo >= 45) {
+
+            message =
+                "⚠️ 震度5弱の揺れです。身の安全を確保してください。";
+
+        } else if (shindo >= 40) {
+
+            message =
+                "⚠️ 震度4の揺れです。揺れに注意してください。";
+
+        } else {
+
+            message =
+                "地震が発生しました。";
+
+        }
+
+        document.getElementById("message").textContent = message;
+
+    } else {
+
+        eart.style.display = "none";
+
+    }
 }
 
 
